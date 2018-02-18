89d29ef8fcf4d70db91de4bb29b4366f852d2589
Matt Horder <matt.horder@outlook.com>
2018-02-18 18:49:39 +0000

    Main menu added

---
+++ UniversityDomination-1.1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load next scene in build order
+        
+    }
+
+    public void QuitGame() {
+
+        Debug.Log("Quit"); // For Testing
+        Application.Quit();
+
+    }
+
+	
+}
+
