882681e523afce1b13535a154db5e805ae05c982
mrchazaaa <charlie.howlett@live.co.uk>
2018-02-19 22:06:30 +0100

    clear master branch
    clearing master branch for final version

--- UniversityDomination-1.1/Assets/Scripts/staticPassArguments.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-static public class staticPassArguments //contains static variable that is passed from main menu to game
-{
-    static public int humanPlayers; //number of human players selected
-    static public bool loadGame; //indicates whether a game is loaded
-}
--- UniversityDomination-1.1/README.md
+++
@@ -1,1 +0,0 @@
-# UniversityDomination - Name pending
