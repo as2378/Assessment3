c1bb8c7add281c5a4f3a27d47485e5e7967d05c9
Devil6Lair <Devil6Lair@Gmail.com>
2018-02-19 23:03:33 +0000

    HeaderCommentLink
    Link to website with executable

--- UniversityDomination/Assets/Scripts/Map.cs
+++ UniversityDomination/Assets/Scripts/Map.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//======================================================
+//Website link with executable:
+//http://www-users.york.ac.uk/~ch1575/documentation
+//======================================================
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
